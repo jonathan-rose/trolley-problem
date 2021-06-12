f6ad429e56a0972e748e4e87a4fc880075c9e0d2
Kimbsy <lordkimber@gmail.com>
2021-06-13 00:51:26 +0100

    Add conveyor belt to title screen
    We've got the nice supermarket ambience going on, it felt fitting to
    have a lazy conveyor belt move some random items along.
    
    Still need to create assets for food items and update the
    purchaseOptions list when done.

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -1,28 +1,55 @@
 import 'phaser';
 import Button from '../Objects/Button';
 
+var indicators;
+var purchases;
+
+// @TODO: create food item assets and put their names here
+var purchaseOptions = ['player', 'trolley', 'redCar'];
+
 export default class TitleScene extends Phaser.Scene {
     constructor () {
 	super('Title');
-    }
-
-    preload () {
     }
 
     create () {
 	var config = this.game.config;
 
         this.add.image(config.width/2, config.height/2, 'menuBG');
-        this.add.image(config.width*0.3, config.height/2, 'Logo');
+        this.add.image(config.width*0.3, config.height/2 - 100, 'Logo');
+
+
+        // Draw conveyor belt
+        this.add.rectangle(config.width*0.5 - 50, config.height*0.85, config.width + 100, 100, 0x394648);
+
+        // Conveyor belt moving indicators
+        indicators = this.physics.add.group();
+        for (var i = 0; i < 3; i++) {
+            var indicator = this.physics.add.sprite(i*(config.width + 100)/3, config.height*0.85, 'conveyorIndicator');
+            indicators.add(indicator);
+            indicator.body.setVelocityX(40);
+        }
+
+        // Items on conveyor belt
+        purchases = this.physics.add.group();
+        for(var i = 0; i < 5; i++) {
+            var purchase = this.physics.add.sprite(
+                i*(config.width + 100)/5 - config.width/2,
+                Phaser.Math.RND.between(config.height*0.82, config.height*0.88),
+                Phaser.Math.RND.pick(purchaseOptions)
+            );
+            purchases.add(purchase);
+            purchase.body.setVelocityX(40);
+        };
 
         // Game - Head to Rocket Select page
-        this.gameButton = new Button(this, config.width*0.75, config.height/2 - 100, 'Button', 'ButtonPressed', 'Play', 'Game');
+        this.gameButton = new Button(this, config.width*0.75, config.height/2 - 200, 'Button', 'ButtonPressed', 'Play', 'Game');
 
         // Options
-        this.optionsButton = new Button(this, config.width*0.75, config.height/2, 'Button', 'ButtonPressed', 'Options', 'Options');
+        this.optionsButton = new Button(this, config.width*0.75, config.height/2 - 100, 'Button', 'ButtonPressed', 'Options', 'Options');
 
         // About
-        this.aboutButton = new Button(this, config.width*0.75, config.height/2 + 100, 'Button', 'ButtonPressed', 'About', 'About');
+        this.aboutButton = new Button(this, config.width*0.75, config.height/2, 'Button', 'ButtonPressed', 'About', 'About');
 
         this.model = this.sys.game.globals.model;
         if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
@@ -45,6 +72,31 @@ export default class TitleScene extends Phaser.Scene {
             },
             callbackScope: ctx,
             loop: true
+        });
+    }
+
+    update () {
+        var config = this.game.config;
+
+        indicators.children.each((indicator) => {
+            if (indicator.x > config.width + 50) {
+                indicator.setX(-50);
+            }
+        });
+
+        // Refresh purchases when they scroll off screen
+        purchases.children.each((purchase) => {
+            if (purchase.x > config.width + 50) {
+                console.log("NEW");
+                purchase.destroy();
+                var newPurchase = this.physics.add.sprite(
+                    Phaser.Math.RND.between(-100, -50),
+                    Phaser.Math.RND.between(config.height*0.82, config.height*0.88),
+                    Phaser.Math.RND.pick(purchaseOptions)
+                );
+                purchases.add(newPurchase);
+                newPurchase.body.setVelocityX(40);
+            }
         });
     }
 };
